2f78a77d8ca4a53d4f89d7880f8aa498ca14da3c
jahir <jahirulislam1156@gmail.com>
2021-03-20 00:58:02 +0600

    Added a new method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(string[]args){
-        System.out.printIn("vs code is awesome")
+        System.out.printIn("vs code is awesome...");
+        greeting("jahir");
+    }
+    public static void greeting(string name){
+        System.out.printIn("Welcome".name);
     }
 }
