a912aa03fc629542aa799cde54536b90c89ac0e6
jahir <jahirulislam1156@gmail.com>
2021-03-20 01:19:55 +0600

    Add a new method called sqr

--- Hello.java
+++ Hello.java
@@ -6,4 +6,7 @@ public class hello{
     public static void greeting(string name){
         System.out.printIn("Welcome".name);
     }
+    public static int sqr(int x){
+        return x*x;
+    }
 }
